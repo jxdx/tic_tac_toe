7f76747a6692b98cb04dde2a27e02c7917846548
JXDX <josh@junglescout.com>
2021-06-06 00:13:46 -0700

    code cleanup

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,6 +1,7 @@
 class TicTacToe
   attr_reader :player, :tic_tac_toe_state
   def initialize
+    # initial state
     @row = 0
     @col = 0
     @player = ''
@@ -12,16 +13,19 @@ class TicTacToe
     @col = col
     @player = player
 
+    # a few small validation checks
     return 'Off the board' if @row > 3 || @col > 3
-    return 'That position is already taken' if !tic_tac_toe_state[@row - 1][@col - 1].nil?
+    return 'That position is already taken' if !tic_tac_toe_state[@row-1][@col-1].nil?
     check_winner
   end
 
   def check_board
+    # print the current stawte
     puts "#{tic_tac_toe_state[0]}, #{tic_tac_toe_state[1]}, #{tic_tac_toe_state[2]}"
   end
 
   def reset_board
+    # reset state
     @tic_tac_toe_state = [[nil,nil,nil],[nil,nil,nil],[nil,nil,nil]]
   end
 
@@ -32,7 +36,7 @@ class TicTacToe
     # check each row if there's a winner aka row win
     tic_tac_toe_state.each do |pos|
       if pos[0] == player && pos[1] == player && pos[2] == player
-        puts 'Row Winner' 
+        puts 'Row Winner, resetting the board' 
         reset_board
       end
     end
@@ -44,7 +48,7 @@ class TicTacToe
       if tic_tac_toe_state[row][col] == player && 
         tic_tac_toe_state[row + 1][col] == player &&
         tic_tac_toe_state[row + 2][col] == player
-        puts 'Column Winner, resetting board'
+        puts 'Column Winner, resetting the board'
         reset_board
       end
       col += 1
@@ -56,7 +60,7 @@ class TicTacToe
       if tic_tac_toe_state[row][col] == player &&
         tic_tac_toe_state[row + 1][col + 1] == player &&
         tic_tac_toe_state[row + 2][col + 2] == player
-          puts 'Diagonal Winner, resetting board' 
+          puts 'Diagonal Winner, resetting the board' 
           reset_board
       end
   end
